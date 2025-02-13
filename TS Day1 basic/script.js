24b699d5d5c88353d270e532b7b70a7bb5acd751
Miss-Nikita <bisennikita770@gmail.com>
2025-02-13 21:47:38 +0530

    DataTypes

--- TS Day1 basic/script.js
+++ TS Day1 basic/script.js
@@ -1,2 +1,5 @@
 // Primitive - Number String Boolean
+// TypeScript :- TypeScript is a syntactic superset of JavaScript which adds static typing.
+// TypeScript allows specifying the types of data being passed around within the code, and has the ability to report errors when the types don't match.
+// For example, TypeScript will report an error when passing a string into a function that expects a number. JavaScript will not.
 console.log("hello from Type script");
