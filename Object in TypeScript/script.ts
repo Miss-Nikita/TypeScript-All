75a7aae31b6e3f30899b5c6a27017c24256388d2
Miss-Nikita <bisennikita770@gmail.com>
2025-03-22 13:16:01 +0530

    Add HTML, CSS, and TypeScript files for basic TypeScript examples; include data types and primitive data explanations

--- Object in TypeScript/script.ts
+++ Object in TypeScript/script.ts
@@ -29,7 +29,7 @@ console.log(person1);
 type ID = number;
 // for multiple variable
 type Product = {
-  readonly id: number | string | boolean;
+  readonly id: number | string | boolean;  //this is known as union type '|' 
   name: string;
   price: number;
   brand: string;
